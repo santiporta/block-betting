ca82822f9b8714ff83a3f3fc0eb4b0266595b52f
Santiago Porta <santiago.porta@darwoft.com>
2024-02-08 14:37:00 -0300

    fix mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
-const path = require('path');
-const express = require('express');
-const config = require('./config');
-const dbConnect = require('./config/db');
-// const connectDB = require('./config/db');
-const configureMiddleware = require('./middleware');
-const configureRoutes = require('./routes');
-const socketio = require('socket.io');
-const gameSocket = require('./socket/index');
+const path = require("path");
+const express = require("express");
+const config = require("./config");
+const connectDB = require("./config/db");
+const configureMiddleware = require("./middleware");
+const configureRoutes = require("./routes");
+const socketio = require("socket.io");
+const gameSocket = require("./socket/index");
 
 // Connect and get reference to mongodb instance
-// let db;
-
-// (async function () {
-//   db = await connectDB();
-// })();
+let db;
 
 // Init express app
 const app = express();
@@ -26,26 +21,21 @@ configureRoutes(app);
 
 // Start server and listen for connections
 const server = app.listen(config.PORT, async () => {
-  await dbConnect()
+  db = await connectDB();
   console.log(
-    `Server is running in ${config.NODE_ENV} mode and is listening on port ${config.PORT}...`,
+    `Server is running in ${config.NODE_ENV} mode and is listening on port ${config.PORT}...`
   );
 });
 
 //  Handle real-time poker game logic with socket.io
 const io = socketio(server);
-console.log(
-  `Connecting socket...`,
-);
-io.on('connect', (socket) => gameSocket.init(socket, io));
-console.log(
-  `Connect socket...`,
-);
+console.log(`Connecting socket...`);
+io.on("connect", (socket) => gameSocket.init(socket, io));
+console.log(`Connect socket...`);
 // Error handling - close server
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   // db.disconnect();
 
   console.error(`Error: ${err.message}`);
-  server.close(() => {
-  });
+  server.close(() => {});
 });
